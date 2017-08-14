8b808111c3441c3f13859fc1b4f82e81cb3efb3b
runphp <runphp@qq.com>
2017-08-14 16:06:03 +0800

    添加自定义模块服务地址

--- src/Mvc/AbstractModule.php
+++ src/Mvc/AbstractModule.php
@@ -91,14 +91,13 @@ abstract class AbstractModule extends Injectable implements ModuleDefinitionInte
         // eelly client service
         $di->setShared('eellyClient', function () {
             $options = $this->getModuleConfig()->oauth2Client->eelly->toArray();
-
             if (ApplicationConst::ENV_PRODUCTION === ApplicationConst::$env) {
-                $eellyClient = EellyClient::init($options);
+                $eellyClient = EellyClient::init($options['options']);
             } else {
                 $collaborators = [
                     'httpClient' => new \GuzzleHttp\Client(['verify' => false]),
                 ];
-                $eellyClient = EellyClient::init($options, $collaborators);
+                $eellyClient = EellyClient::init($options['options'], $collaborators, $options['providerUri']);
             }
             $eellyClient->getProvider()->setAccessTokenCache($this->getCache());
 
