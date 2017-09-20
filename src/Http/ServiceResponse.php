e9bc3184f8525944cf4546e4207bc9f5792c0a44
runphp <runphp@qq.com>
2017-09-20 15:29:25 +0800

    去除content-type

--- src/Http/ServiceResponse.php
+++ src/Http/ServiceResponse.php
@@ -22,7 +22,6 @@ class ServiceResponse extends Response
 {
     public function afterServiceResolve(): void
     {
-        $this->setContentType('application/json');
         $this->setHeader('Access-Control-Allow-Origin', '*');
         $this->setHeader('Server', ApplicationConst::APP_NAME.'/'.ApplicationConst::APP_VERSION);
         $this->setStatusCode(200);
