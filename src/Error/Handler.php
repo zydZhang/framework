23bead6c57a19adbed2a5d171f3f7d95e1dc8639
runphp <runphp@qq.com>
2017-11-15 16:26:56 +0800

    代码还原

--- src/Error/Handler.php
+++ src/Error/Handler.php
@@ -87,7 +87,6 @@ class Handler extends Injectable
     {
         if (null === $this->logger) {
             $di = $this->getDI();
-            class_exists()
             $this->logger = $di->getLogger();
             if ('php' == APP['env']) {
                 $streamHandler = new StreamHandler('php://stdout');
