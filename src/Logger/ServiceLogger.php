9baf2b9ae65d3504142a634cfc5b53e6f00c2695
heui <runphp@qq.com>
2017-12-16 03:07:53 +0000

    Apply fixes from StyleCI

--- src/Logger/ServiceLogger.php
+++ src/Logger/ServiceLogger.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace Eelly\Logger;
 
-use Eelly\Application\ApplicationConst;
 use Eelly\Logger\Handler\DingDingHandler;
 use Monolog\Logger;
 use Phalcon\Di\InjectionAwareInterface;
