7857919c96bc7abfd1317b0d299fc8c48fb673d3
runphp <runphp@qq.com>
2018-01-03 17:00:09 +0800

    去除非框架的代码

--- src/Mvc/WebController.php
+++ src/Mvc/WebController.php
@@ -16,33 +16,7 @@ namespace Shadon\Mvc;
 /**
  * Class WebController.
  *
- * @property \Phalcon\Session\Adapter $session
  */
 class WebController extends Controller
 {
-    public function onConstruct(): void
-    {
-        // add cache service
-        $this->di->setShared('cache', function () {
-            $config = $this->getConfig()->cache->toArray();
-            $frontend = $this->get($config['frontend'], [$config['options'][$config['frontend']]]);
-
-            return $this->get($config['backend'], [$frontend, $config['options'][$config['backend']]]);
-        });
-    }
-
-    /**
-     * display other template.
-     *
-     * @param string $controller template name
-     * @param string $action     action name
-     */
-    public function sendTemplateRender($controller, $action): void
-    {
-        $this->view->render(
-            $controller,
-            $action
-        );
-        $this->response->setContent($this->view->getContent());
-    }
 }
