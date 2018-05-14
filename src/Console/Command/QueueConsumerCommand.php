e38d8efec5df2354ede5c16b3372932fe07b2878
runphp <runphp@qq.com>
2018-05-14 18:53:17 +0800

    队列异常日志修改

--- src/Console/Command/QueueConsumerCommand.php
+++ src/Console/Command/QueueConsumerCommand.php
@@ -148,9 +148,16 @@ class QueueConsumerCommand extends SymfonyCommand implements InjectionAwareInter
                     $worker->write(sprintf('%s %d -1 "%s"', DateTime::formatTime(), $pid, $e->getMessage()));
                     sleep(random_int(1, 10));
                     $consumer = $worker->createConsumer($exchange, $routingKey, $queue);
-                } catch (\Throwable | \Exception $e) {
-                    $worker->write(sprintf('%s %d -1 "%s"', DateTime::formatTime(), $pid, $e->getMessage()));
-                    $this->di->getShared('logger')->info($e->getMessage());
+                } catch (\Throwable | \Exception $exception) {
+                    $worker->write(sprintf('%s %d -1 "%s"', DateTime::formatTime(), $pid, $exception->getMessage()));
+                    $this->di->getShared('logger')->error('UncaughtException', [
+                        'file'  => $exception->getFile(),
+                        'line'  => $exception->getLine(),
+                        'class' => get_class($exception),
+                        'args'  => [
+                            $exception->getMessage(),
+                        ],
+                    ]);
                     break;
                 }
             }
