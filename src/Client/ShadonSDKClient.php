b7b3b4665e28cca58615ed07122bea0cfae2ed25
hehui <runphp@qq.com>
2018-09-03 11:25:32 +0800

    Fix unimport interface #111

--- src/Client/ShadonSDKClient.php
+++ src/Client/ShadonSDKClient.php
@@ -18,6 +18,7 @@ use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\MultipartStream;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Token\AccessToken;
+use Psr\Http\Message\UploadedFileInterface;
 use Shadon\OAuth2\Client\Provider\ShadonProvider;
 
 /**
